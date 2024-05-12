ff27128ccb4acc76e92e73ffca366c7cc973ab45
Kyle Lee <kyle@kylelee.dev>
2024-05-12 02:40:03 -0500

    changed font to jetbrains mono

--- wezterm/fonts.lua
+++ wezterm/fonts.lua
@@ -3,8 +3,8 @@ local font_config = {}
 
 function font_config.get_fonts()
 	return wezterm.font_with_fallback({
-		{ family = "Hack Nerd Font Mono", weight = "Medium" },
 		{ family = "JetBrains Mono", weight = "Medium" },
+		{ family = "Hack Nerd Font Mono", weight = "Medium" },
 		{ family = "Iosevka Nerd Font Mono", weight = "Medium" },
 		{ family = "SauceCodePro Nerd Font Mono", weight = "Medium" },
 		{ family = "FiraCode Nerd Font Mono", weight = "Regular" },
